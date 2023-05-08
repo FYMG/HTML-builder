979face573b48a50b416b70e324ad39253020f15
FYMG <34478249+FYMG@users.noreply.github.com>
2023-05-08 11:28:22 +0300

    fix: add check for absolute path and recursive folder copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,20 +3,24 @@ const fs = require('fs');
 const dir = path.join(path.dirname(__dirname), '04-copy-directory');
 
 const copyDirectory = (directory, directoryCopy) => {
-  const dirPath = path.join(dir, directory);
-  const dirCopyPath = path.join(dir, directoryCopy);
+  const dirPath = path.isAbsolute(directory) ? directory : path.join(dir, directory);
+  const dirCopyPath = path.isAbsolute(directory) ? directoryCopy : path.join(dir, directoryCopy);
   fs.rm(dirCopyPath, {recursive: true, force: true}, (err) => {
     if (err) throw err;
     fs.mkdir(dirCopyPath,{recursive:true} , (err) => {
       if (err) throw err;
-      fs.readdir(dirPath, (err, files) => {
+      fs.readdir(dirPath, {withFileTypes: true}, (err, files) => {
         if (err) throw err;
         files.forEach((file) => {
-          const filePath = path.join(dirPath, file);
-          const newFilePath = path.join(dirCopyPath, file);
-          fs.copyFile(filePath, newFilePath, (err) => {
-            if (err) throw err;
-          });
+          const filePath = path.join(dirPath, file.name);
+          const newFilePath = path.join(dirCopyPath, file.name);
+          if (file.isDirectory()){
+            copyDirectory(filePath, newFilePath);
+          }else {
+            fs.copyFile(filePath, newFilePath, (err) => {
+              if (err) throw err;
+            });
+          }
         });
       });
     });
