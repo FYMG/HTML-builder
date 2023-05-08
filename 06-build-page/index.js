9be087470dce62d1fc605f5766114f9f3bdab05b
FYMG <34478249+FYMG@users.noreply.github.com>
2023-05-08 13:25:24 +0300

    feat: add template merge function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -13,11 +13,31 @@ const createResultFolder = async (resultDir) => {
     });
 };
 
-const mergeStyles = (stylesDir, resultDir, resultName = 'bundle.css') => {
-  const stylesPath = path.isAbsolute(stylesDir) ? stylesDir : path.join(dir, stylesDir);
-  const resultPath = path.isAbsolute(resultDir) ? resultDir : path.join(dir, resultDir);
-  fs.unlink(path.join(resultPath, resultName), () => {
-    fs.writeFile(path.join(resultPath, resultName), '', (err) => {
+const getComponentContent = (componentPath) => {
+  componentPath = path.isAbsolute(componentPath) ? componentPath : path.join(dir, componentPath);
+  return fs.promises.readFile(componentPath, {encoding: 'utf8'});
+};
+
+const mergeTemplate = async (template, componentsDir, result) => {
+  template = path.isAbsolute(template) ? template : path.join(dir, template);
+  componentsDir = path.isAbsolute(componentsDir) ? componentsDir : path.join(dir, componentsDir);
+  result = path.isAbsolute(result) ? result : path.join(dir, result);
+  const components = await fs.promises.readdir(componentsDir, {withFileTypes: true})
+    .then((files) => {
+      return files.filter(x => x.isFile() && path.extname(x.name) === '.html').map(x => x.name);
+    });
+  let templateContent = await fs.promises.readFile(template, {encoding: 'utf8'});
+  for (const component of components) {
+    const re = new RegExp(`{{${path.basename(path.parse(component).name)}}}`, 'gi');
+    templateContent = templateContent.replace(re, await getComponentContent(path.join(componentsDir, component)));
+  }
+  await fs.promises.writeFile(result, templateContent);
+};
+const mergeStyles = (fromDir, to) => {
+  const stylesPath = path.isAbsolute(fromDir) ? fromDir : path.join(dir, fromDir);
+  const resultPath = path.isAbsolute(to) ? to : path.join(dir, to);
+  fs.unlink(resultPath, () => {
+    fs.writeFile(resultPath, '', (err) => {
       if (err) throw err;
       fs.readdir(stylesPath, {withFileTypes: true}, (err, files) => {
         if (err) throw err;
@@ -25,11 +45,11 @@ const mergeStyles = (stylesDir, resultDir, resultName = 'bundle.css') => {
           if (path.extname(file.name) === '.css' && file.isFile()) {
             const readStream = fs.createReadStream(path.join(stylesPath, file.name));
             readStream.on('data', (chunk) => {
-              fs.appendFile(path.join(resultPath, resultName), chunk, (err) => {
+              fs.appendFile(resultPath, chunk, (err) => {
                 if (err) throw err;
               });
             });
-            readStream.on('end', () => {readStream.close();});
+            readStream.on('end', () => readStream.close());
           }
         });
       });
@@ -37,9 +57,9 @@ const mergeStyles = (stylesDir, resultDir, resultName = 'bundle.css') => {
   });
 };
 
-const copyDirectory = (directory, directoryCopy) => {
-  const dirPath = path.isAbsolute(directory) ? directory : path.join(dir, directory);
-  const dirCopyPath = path.isAbsolute(directory) ? directoryCopy : path.join(dir, directoryCopy);
+const copyDirectory = (from, to) => {
+  const dirPath = path.isAbsolute(from) ? from : path.join(dir, from);
+  const dirCopyPath = path.isAbsolute(from) ? to : path.join(dir, to);
   fs.rm(dirCopyPath, {recursive: true, force: true}, (err) => {
     if (err) throw err;
     fs.mkdir(dirCopyPath,{recursive:true} , (err) => {
@@ -64,8 +84,9 @@ const copyDirectory = (directory, directoryCopy) => {
 
 const builder = async (resultDir) => {
   await createResultFolder(resultDir);
-  mergeStyles('styles', 'project-dist', 'style.css');
+  mergeStyles('styles', 'project-dist/style.css');
   copyDirectory('assets', 'project-dist/assets');
+  mergeTemplate('template.html', 'components', './project-dist/index.html');
 };
 
 builder('project-dist');
